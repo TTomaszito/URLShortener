5357badc1d06b5cab7735b5f39f90f31e3aff8e2
Ttomaszito <ttomaszito@gmail.com>
2016-12-22 21:48:49 -0500

    proc file

--- app.js
+++ app.js
@@ -72,6 +72,6 @@ MongoClient.connect(url, function (err, db) {
 });
 
 
-app.listen(3000, function () {
+app.listen(process.env.PORT, function () {
   console.log('Example app listening on port 3000!')
 })
